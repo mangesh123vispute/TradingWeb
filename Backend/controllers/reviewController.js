c36abf3f1434504c585930a22af60611b8f298f7
mangesh vispute <mangesh2003vispute@gmail.com>
2024-05-12 11:42:25 +0530

    CRUD on the Review is completed

--- Backend/controllers/reviewController.js
+++ Backend/controllers/reviewController.js
@@ -1,7 +1,8 @@
 import Review from "../models/review.models.js";
 import User from "../models/user.models.js";
+import mongoose from "mongoose";
 
-//* Rating and Review
+//* Create Rating and Review
 // Take user id , content and rating form the frontend
 // check if user is registerd or not
 // check if user already reviewed or not
@@ -49,6 +50,142 @@ export const createReview = async (req, res) => {
     res.status(401).json({
       status: "fail",
       message: err?.message,
+    });
+  }
+};
+
+//* Get Rating and Review
+//algo
+//get all reviews
+//send them to frontend
+// (accessable to all )
+export const getReview = async (req, res) => {
+  try {
+    const allReviews = await Review.find();
+    res.status(200).json({
+      status: "success",
+      data: {
+        reviews: allReviews,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Failed to fetch reviews",
+      error: err.message,
+    });
+  }
+};
+
+//* update review
+// algo
+//check if user is login or not
+//get the id of the review form the params
+//check if the id is in the database
+// get all the data from the request body
+//check if content is not empty
+// if not empty then update the review
+// send response
+
+export const updateReview = async (req, res) => {
+  try {
+    const { id } = req.params; // Extracting ID from request parameters
+    const { content, rating } = req.body;
+
+    // Check if all required fields are present
+    if (!(content && rating)) {
+      return res
+        .status(400)
+        .json({ status: "fail", message: "All fields are required" });
+    }
+
+    //chacking if rating is between 1 to 5
+    if (rating < 1 || rating > 5) {
+      return res
+        .status(405)
+        .json({ status: "fail", message: "Rating must be between 1 to 5" });
+    }
+
+    // Check if the provided ID is valid
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res
+        .status(400)
+        .json({ status: "fail", message: "Invalid review ID" });
+    }
+
+    // Check if the review exists
+    const existingReview = await Review.findById(id);
+    if (!existingReview) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "Review not found" });
+    }
+
+    // check if review object user and logged in user are same
+    if (existingReview.userId.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ status: "fail", message: "You are not authorized" });
+    }
+    // Update the review with the new data
+    existingReview.content = content;
+    existingReview.rating = rating;
+    await existingReview.save();
+    // Send the updated review as a response
+    res.status(200).json({
+      status: "success",
+      data: {
+        review: existingReview,
+      },
+    });
+  } catch (err) {
+    // Handle errors
+    res.status(500).json({
+      status: "error",
+      message: "Failed to update review",
+      error: err.message,
+    });
+  }
+};
+
+//* Delete review
+//algo
+//check if user is login or not
+//get the id of the review form the params
+//delete the review
+// send response
+
+export const deleteReview = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const existingReview = await Review.findById(id);
+
+    if (!existingReview) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "Review not found" });
+    }
+
+    if (existingReview.userId.toString() !== req.user._id.toString()) {
+      return res.status(403).json({
+        status: "fail",
+        message: "You are not authorized, You can only delete your review",
+      });
+    }
+
+    await Review.findByIdAndDelete(id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        review: existingReview,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Failed to delete review",
+      error: err.message,
     });
   }
 };
--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -1,11 +1,22 @@
 import express from "express";
 const router = express.Router();
 import { register, login } from "../controllers/userController.js";
-import { createReview } from "../controllers/reviewController.js";
+import {
+  createReview,
+  getReview,
+  updateReview,
+  deleteReview,
+} from "../controllers/reviewController.js";
 import { verifyJWT } from "../middelwares/auth.middleware.js";
 
+//* Authentication
 router.route("/users/register").post(register);
 router.route("/users/login").post(login);
-router.route("/users/review").post(verifyJWT, createReview);
+
+//* Review Crud
+router.route("/users/review/create").post(verifyJWT, createReview);
+router.route("/users/review/fetchall").get(getReview);
+router.route("/users/review/update/:id").put(verifyJWT, updateReview);
+router.route("/users/review/delete/:id").delete(verifyJWT, deleteReview);
 
 export default router;
